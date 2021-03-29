233553309951293feec4197744a12f2ab624ee8d
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-03-29 04:39:19 +0000

    0.0.1

---
+++ imports/react-token.tsx
@@ -0,0 +1,12 @@
+import uniqid from 'uniqid';
+import { TokenContext } from '@deepcase/react-hasura/token-context';
+import { useLocalStore } from '@deepcase/store/local';
+
+export function useTokenController() {
+  return useLocalStore('dc-dg-token', '');
+}
+
+export function TokenProvider({ children }: { children?: any }) {
+  const [token, setToken] = useTokenController();
+  return <TokenContext.Provider value={token}>{children}</TokenContext.Provider>;
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # deepcase deepgraph
+
+[![npm version](https://badge.fury.io/js/%40deepcase%2Fdeepgraph.svg)](https://badge.fury.io/js/%40deepcase%2Fdeepgraph) 
+[![develop deepcase](https://badgen.net/badge/develop/deepcase)](https://github.com/deepcase/deepcase)
