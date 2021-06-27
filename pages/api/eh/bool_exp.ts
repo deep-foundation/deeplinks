edc4d63d0ef313e156b34f3123ee3fecaf8916d3
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-06-27 15:59:07 +0000

    https://github.com/deepcase/deepcase/issues/45

--- pages/api/eh/bool_exp.ts
+++ pages/api/eh/bool_exp.ts
@@ -16,46 +16,6 @@ const client = generateApolloClient({
   secret: process.env.MIGRATIONS_HASURA_SECRET,
 });
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-
-// function echo(event) {
-//    let responseBody = '';
-//     if (event.op === "INSERT") {
-//         responseBody = `New user ${event.data.new.id} inserted, with data: ${event.data.new.name}`;
-//     }
-//     else if (event.op === "UPDATE") {
-//         responseBody = `User ${event.data.new.id} updated, with data: ${event.data.new.name}`;
-//     }
-//     else if (event.op === "DELETE") {
-//         responseBody = `User ${event.data.old.id} deleted, with data: ${event.data.old.name}`;
-//     }
-
-//     return responseBody;
-// };
-
-// app.use(bodyParser.json());
-
-// app.post('/', function (req, res) {
-//     try{
-//         var result = echo(req.body.event);
-//         res.json(result);
-//     } catch(e) {
-//         console.log(e);
-//         res.status(500).json(e.toString());
-//     }
-// });
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World - For Event Triggers, try a POST request?');
-// });
-
-// var server = app.listen(process.env.PORT, function () {
-//     console.log("server listening");
-// });
-
 const cors = Cors({ methods: ['GET', 'HEAD', 'POST'] });
 export default async (req, res) => {
   await corsMiddleware(req, res, cors);
