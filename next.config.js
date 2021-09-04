301c6a35ed41653d44e3dd10699da6e6ecf6cadf
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-09-04 14:52:53 +0000

    0.0.11

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+ 
+dotenvLoad();
+ 
+const withNextEnv = nextEnv();
+ 
+module.exports = withNextEnv({
   distDir: 'app',
-} 
+});
