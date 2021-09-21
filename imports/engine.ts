8a01738342d949b018d715c3709ab2248d7825f9
ivansglazunov <ivansglazunov@gmail.com>
2021-09-21 06:56:20 +0300

    0.0.24

--- imports/engine.ts
+++ imports/engine.ts
@@ -5,6 +5,7 @@ const execP = promisify(exec);
 
 export interface IOptions {
   operation: 'run' | 'sleep' | 'reset';
+  path: string;
 }
 
 const {
@@ -34,17 +35,17 @@ const _deeplinks = `${__dirname}/../`;
 export async function call (options: IOptions) {
   console.log('call', options);
   if (options.operation === 'run') {
-    const { stdout, stderr } = await execP(`((cd ${_hasura}/local/; npm run docker); sleep 10; (cd ${_deeplinks}; npm run migrate))`);
+    const { stdout, stderr } = await execP(`export PATH=$PATH:${options.path};((cd ${_hasura}/local/; npm run docker); sleep 10; (cd ${_deeplinks}; npm run migrate))`);
     console.log(stdout);
     console.log(stderr);
   }
   if (options.operation === 'sleep') {
-    const { stdout, stderr } = await execP(`(cd ${_hasura}/local/; docker-compose down;)`);
+    const { stdout, stderr } = await execP(`export PATH=$PATH:${options.path};(cd ${_hasura}/local/; docker-compose down;)`);
     console.log(stdout);
     console.log(stderr);
   }
   if (options.operation === 'reset') {
-    const { stdout, stderr } = await execP(`((cd ${_hasura}/local/; docker rm -f $(docker ps -a -q); docker volume rm $(docker volume ls -q)); (cd ${_deeplinks}; find . -type f -name '.migrate' -exec rm {} +))`);
+    const { stdout, stderr } = await execP(`export PATH=$PATH:${options.path};((cd ${_hasura}/local/; docker rm -f $(docker ps -a -q); docker volume rm $(docker volume ls -q)); (cd ${_deeplinks}; find . -type f -name '.migrate' -exec rm {} +))`);
     console.log(stdout);
     console.log(stderr);
   }
