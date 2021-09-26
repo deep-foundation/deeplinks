75b4b22b5aefd77c6b82897a4614c8ecf5e7b616
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-09-26 17:19:44 +0300

    0.0.29

--- imports/engine.ts
+++ imports/engine.ts
@@ -1,5 +1,6 @@
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import path from 'path';
 
 const execP = promisify(exec);
 
@@ -8,27 +9,30 @@ export interface IOptions {
   handle?: (exec: string) => string;
 }
 
-const _hasura = `${__dirname}/../../hasura`;
-const _deeplinks = `${__dirname}/../`;
+const _hasura = path.normalize(`${__dirname}/../../hasura`);
+const _deeplinks = path.normalize(`${__dirname}/../`);
 
 export async function call (options: IOptions) {
   if (options.operation === 'run') {
-    let str = `((cd ${_hasura}/local/ && npm run docker) && sleep 10 && (cd ${_deeplinks} && npm run migrate))`;
+    let str = `cd ${path.normalize(`${_hasura}/local/`)} && npm run docker && cd ${__dirname} && wait-on tcp:8080 && cd ${_deeplinks} && npm run migrate`;
     str = options.handle ? options.handle(str) : str;
+    console.log(str);
     const { stdout, stderr } = await execP(str);
     console.log(stdout);
     console.log(stderr);
   }
   if (options.operation === 'sleep') {
-    let str = `(cd ${_hasura}/local/ && docker-compose down)`;
+    let str = `cd ${path.normalize(`${_hasura}/local/`)} && docker-compose down && cd ${__dirname}`;
     str = options.handle ? options.handle(str) : str;
+    console.log(str);
     const { stdout, stderr } = await execP(str);
     console.log(stdout);
     console.log(stderr);
   }
   if (options.operation === 'reset') {
-    let str = `((cd ${_hasura}/local/ && docker rm -f $(docker ps -a -q) && docker volume rm $(docker volume ls -q)) && (cd ${_deeplinks} && find . -type f -name '.migrate' -exec rm {} +))`;
+    let str = `cd ${path.normalize(`${_hasura}/local/`)} && docker-compose down && cd ${__dirname} && docker container prune -f && docker system prune --volumes -f && cd ${_deeplinks} && rimraf .migrate`;
     str = options.handle ? options.handle(str) : str;
+    console.log(str);
     const { stdout, stderr } = await execP(str);
     console.log(stdout);
     console.log(stderr);
