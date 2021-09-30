5873ccee0a7b02c27c4854faeaac61e1fe433a79
Пётр Безденежных <menzorg@gmail.com>
2021-09-30 12:16:58 +0300

    proxy

--- start.js
+++ start.js
@@ -3,56 +3,57 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 var express = require('express');
 var app = express();
 
-app.use('/hasura/v1/graphql', createProxyMiddleware({ target: 'http://localhost:8080/v1/graphql', ws: true, changeOrigin: true }));
-app.use('/hasura/console', createProxyMiddleware({ target: 'http://localhost:8080/console', changeOrigin: true }));
-app.use('/', createProxyMiddleware({ target: `http://localhost:${process.env.NEXTPORT}`, changeOrigin: true }));
+app.use('/hasura', createProxyMiddleware({
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+  ws:true,
+  pathRewrite: {
+    "^/hasura": "/",
+  }
+}));
 
 app.listen(process.env.PORT, () => {
   console.log(`Hello bugfixers! Wrapped app listening at ${process.env.PORT} port`);
 })
 
-const url = execSync('echo -n $DATABASE_URL', { encoding: 'utf-8' });
-const gql = spawn('./graphql-engine', ['serve'], {
-  env: {
-    ...process.env,
-    HASURA_GRAPHQL_DATABASE_URL: url,
-    HASURA_GRAPHQL_ENABLE_CONSOLE: true,
-    HASURA_GRAPHQL_ADMIN_SECRET: 'myadminsecretkey',
-    HASURA_GRAPHQL_DEV_MODE: 'true',
-    HASURA_GRAPHQL_LOG_LEVEL: 'debug',
-  }
-});
+// const url = execSync('echo -n $DATABASE_URL', { encoding: 'utf-8' });
+// const gql = spawn('./graphql-engine', ['serve'], {
+//   env: {
+//     ...process.env,
+//     HASURA_GRAPHQL_DATABASE_URL: url,
+//   }
+// });
 
-const deeplinksApp = spawn('npm', ['run', 'heroku-next-start'], {
-    env: {
-      ...process.env,
-      PORT: process.env.NEXTPORT
-    }
-  });
+// const deeplinksApp = spawn('npm', ['run', 'heroku-next-start'], {
+//     env: {
+//       ...process.env,
+//       PORT: process.env.NEXTPORT
+//     }
+//   });
 
-gql.stdout.on('data', (data) => {
-  console.log(`{ "logtype": "hasura", "log": ${data}`);
-});
+// gql.stdout.on('data', (data) => {
+//   console.log(`{ "logtype": "hasura", "log": ${data}`);
+// });
 
-gql.stderr.on('data', (data) => {
-  console.log(`{ "logtype": "hasura", "error": ${data}`);
-});
+// gql.stderr.on('data', (data) => {
+//   console.log(`{ "logtype": "hasura", "error": ${data}`);
+// });
 
-gql.on('close', (code) => {
-  console.log(`gql exited with code ${code}`);
-});
+// gql.on('close', (code) => {
+//   console.log(`gql exited with code ${code}`);
+// });
 
-deeplinksApp.stdout.on('data', (data) => {
- console.log(`{ "logtype": "app", "log": ${data}`);
-});
+// deeplinksApp.stdout.on('data', (data) => {
+//  console.log(`{ "logtype": "app", "log": ${data}`);
+// });
 
-deeplinksApp.stderr.on('data', (data) => {
-  console.log(`{ "logtype": "app", "error": ${data}`);
-});
+// deeplinksApp.stderr.on('data', (data) => {
+//   console.log(`{ "logtype": "app", "error": ${data}`);
+// });
 
-deeplinksApp.on('close', (code) => {
-  console.log(`deeplinksApp exited with code ${code}`);
-});
+// deeplinksApp.on('close', (code) => {
+//   console.log(`deeplinksApp exited with code ${code}`);
+// });
 
 // let migrations;
 // setTimeout(()=>{
