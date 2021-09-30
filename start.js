0c7c8920921fe6cc23c2c7c2b8f0e36c7fa61611
Пётр Безденежных <menzorg@gmail.com>
2021-09-30 16:23:01 +0300

    next route

--- start.js
+++ start.js
@@ -11,49 +11,49 @@ app.use('/hasura', createProxyMiddleware({
     "^/hasura": "/",
   }
 }));
-
+app.use('/', createProxyMiddleware({ target: `http://localhost:${process.env.NEXTPORT}`, changeOrigin: true }));
 app.listen(process.env.PORT, () => {
   console.log(`Hello bugfixers! Wrapped app listening at ${process.env.PORT} port`);
 })
 
-// const url = execSync('echo -n $DATABASE_URL', { encoding: 'utf-8' });
-// const gql = spawn('./graphql-engine', ['serve'], {
-//   env: {
-//     ...process.env,
-//     HASURA_GRAPHQL_DATABASE_URL: url,
-//   }
-// });
+const url = execSync('echo -n $DATABASE_URL', { encoding: 'utf-8' });
+const gql = spawn('./graphql-engine', ['serve'], {
+  env: {
+    ...process.env,
+    HASURA_GRAPHQL_DATABASE_URL: url,
+  }
+});
 
-// const deeplinksApp = spawn('npm', ['run', 'heroku-next-start'], {
-//     env: {
-//       ...process.env,
-//       PORT: process.env.NEXTPORT
-//     }
-//   });
+const deeplinksApp = spawn('npm', ['run', 'heroku-next-start'], {
+    env: {
+      ...process.env,
+      PORT: process.env.NEXTPORT
+    }
+  });
 
-// gql.stdout.on('data', (data) => {
-//   console.log(`{ "logtype": "hasura", "log": ${data}`);
-// });
+gql.stdout.on('data', (data) => {
+  console.log(`{ "logtype": "hasura", "log": ${data}`);
+});
 
-// gql.stderr.on('data', (data) => {
-//   console.log(`{ "logtype": "hasura", "error": ${data}`);
-// });
+gql.stderr.on('data', (data) => {
+  console.log(`{ "logtype": "hasura", "error": ${data}`);
+});
 
-// gql.on('close', (code) => {
-//   console.log(`gql exited with code ${code}`);
-// });
+gql.on('close', (code) => {
+  console.log(`gql exited with code ${code}`);
+});
 
-// deeplinksApp.stdout.on('data', (data) => {
-//  console.log(`{ "logtype": "app", "log": ${data}`);
-// });
+deeplinksApp.stdout.on('data', (data) => {
+ console.log(`{ "logtype": "app", "log": ${data}`);
+});
 
-// deeplinksApp.stderr.on('data', (data) => {
-//   console.log(`{ "logtype": "app", "error": ${data}`);
-// });
+deeplinksApp.stderr.on('data', (data) => {
+  console.log(`{ "logtype": "app", "error": ${data}`);
+});
 
-// deeplinksApp.on('close', (code) => {
-//   console.log(`deeplinksApp exited with code ${code}`);
-// });
+deeplinksApp.on('close', (code) => {
+  console.log(`deeplinksApp exited with code ${code}`);
+});
 
 // let migrations;
 // setTimeout(()=>{
