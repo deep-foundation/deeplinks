c9d6838c70b174cc4c139ce9e0a6096c62df36dd
Konstantin Dyachenko <drakonard@gmail.com>
2022-01-09 17:42:14 +0000

    Promise trigger code is now separate from values table code. https://github.com/deep-foundation/deeplinks/issues/7

--- imports/type-table.ts
+++ imports/type-table.ts
@@ -31,29 +31,6 @@ export const generateUp = (options: ITypeTableStringOptions) => async () => {
     ${/* Should we add customIndexesSql? */ customColumnsSql ? /*customColumnsSql*/ '' : `CREATE INDEX IF NOT EXISTS ${tableName}__value_btree ON ${tableName} USING btree (value);`}
     ${customAfterSql}
   `);
-  const promiseTypeId = await deep.id('@deep-foundation/core', 'Promise');
-  const thenTypeId = await deep.id('@deep-foundation/core', 'Then');
-  const handleUpdateTypeId = await deep.id('@deep-foundation/core', 'HandleUpdate');
-  await api.sql(sql`CREATE OR REPLACE FUNCTION ${tableName}__promise__insert__function() RETURNS TRIGGER AS $trigger$ DECLARE PROMISE bigint;
-  BEGIN
-    IF (
-        EXISTS(
-          SELECT 1
-          FROM links handle_update, links updated_link
-          WHERE
-              updated_link.id = NEW."link_id"
-          AND handle_update.from_id = updated_link."type_id"
-          AND handle_update.type_id = 52 -- ${handleUpdateTypeId}
-        )
-    ) THEN
-    --  INSERT INTO links ("type_id", "from_id", "to_id") VALUES (777, NEW."link_id", ${thenTypeId} + 1);
-
-    -- INSERT INTO links ("type_id") VALUES (${promiseTypeId}) RETURNING id INTO PROMISE;
-    -- INSERT INTO links ("type_id","from_id","to_id") VALUES (${thenTypeId},NEW."link_id",PROMISE);
-    END IF;
-    RETURN NEW;
-  END; $trigger$ LANGUAGE plpgsql;`);
-  await api.sql(sql`CREATE TRIGGER ${tableName}__promise__insert__trigger AFTER INSERT ON "${tableName}" FOR EACH ROW EXECUTE PROCEDURE ${tableName}__promise__insert__function();`);
   await api.query({
     type: 'track_table',
     args: {
@@ -155,9 +132,42 @@ export const generateDown = (options: ITypeTableStringOptions) => async () => {
       },
     },
   });
-  await api.sql(sql`DROP TRIGGER IF EXISTS ${tableName}__promise__insert__trigger ON "${tableName}";`);
-  await api.sql(sql`DROP FUNCTION IF EXISTS ${tableName}__promise__insert__function CASCADE;`);
   await api.sql(sql`
     DROP TABLE ${schemaName}."${tableName}";
   `);
+};
+
+export const promiseTriggerUp = (options: ITypeTableStringOptions) => async () => {
+  const { schemaName, tableName, valueType, customColumnsSql = '', customAfterSql = '', linkRelation, linksTableName, api, deep } = options;
+
+  const promiseTypeId = await deep.id('@deep-foundation/core', 'Promise');
+  const thenTypeId = await deep.id('@deep-foundation/core', 'Then');
+  const handleUpdateTypeId = await deep.id('@deep-foundation/core', 'HandleUpdate');
+  await api.sql(sql`CREATE OR REPLACE FUNCTION ${tableName}__promise__insert__function() RETURNS TRIGGER AS $trigger$ DECLARE PROMISE bigint;
+  BEGIN
+    IF (
+        EXISTS(
+          SELECT 1
+          FROM links handle_update, links updated_link
+          WHERE
+              updated_link.id = NEW."link_id"
+          AND handle_update.from_id = updated_link."type_id"
+          AND handle_update.type_id = 52 -- ${handleUpdateTypeId}
+        )
+    ) THEN
+    --  INSERT INTO links ("type_id", "from_id", "to_id") VALUES (777, NEW."link_id", ${thenTypeId} + 1);
+
+    -- INSERT INTO links ("type_id") VALUES (${promiseTypeId}) RETURNING id INTO PROMISE;
+    -- INSERT INTO links ("type_id","from_id","to_id") VALUES (${thenTypeId},NEW."link_id",PROMISE);
+    END IF;
+    RETURN NEW;
+  END; $trigger$ LANGUAGE plpgsql;`);
+  await api.sql(sql`CREATE TRIGGER ${tableName}__promise__insert__trigger AFTER INSERT ON "${tableName}" FOR EACH ROW EXECUTE PROCEDURE ${tableName}__promise__insert__function();`);
+};
+
+export const promiseTriggerDown = (options: ITypeTableStringOptions) => async () => {
+  const { schemaName, tableName, linkRelation, linksTableName, api, deep } = options;
+  
+  await api.sql(sql`DROP TRIGGER IF EXISTS ${tableName}__promise__insert__trigger ON "${tableName}";`);
+  await api.sql(sql`DROP FUNCTION IF EXISTS ${tableName}__promise__insert__function CASCADE;`);
 };
