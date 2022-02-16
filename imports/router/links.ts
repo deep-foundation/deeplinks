fe8a22c76bf2f07a48bc5dd22b2e17ce367d4e55
Konstantin Dyachenko <drakonard@gmail.com>
2022-02-16 14:05:08 +0000

    Context for copilot is removed. https://github.com/deep-foundation/deeplinks/issues/7

--- imports/router/links.ts
+++ imports/router/links.ts
@@ -89,54 +89,6 @@ export const handlerOperations = {
   Delete: 'HandleDelete',
 };
 
-// More context for Copilot:
-//
-// async useHandler( options: handlerOptions ): Promise<{ error?: string; }> {
-//   const { handler, port } = options;
-//   const { network, handlersHash, portsHash, docker, gqlURN } = this;
-//   console.log({ handlersHash, portsHash });
-//   const containerName = crypto.createHash('md5').update(handler).digest("hex");
-//   if (handlersHash[containerName] && portsHash[handlersHash[containerName]] !== 'broken') return this.callHandler({ ...options, port: handlersHash[containerName] });
-//   let dockerPort = port || await getPort();
-//   while (portsHash[port]) {
-//     dockerPort = await getPort();
-//   }
-//   portsHash[dockerPort] = containerName;
-//   handlersHash[containerName] = dockerPort;
-//   const startDocker = `docker run -e PORT=${dockerPort} -e GQL_URN=${gqlURN} -e GQL_SSL=0 --name ${containerName} ${docker ? `--expose ${dockerPort}` : `-p ${dockerPort}:${dockerPort}` } --net ${network} -d ${handler} && npx wait-on http://${docker ? portsHash[dockerPort] : 'localhost'}:${dockerPort}/healthz`;
-//   console.log('startDocker', { startDocker });
-//   let startResult;
-//   try {
-//     startResult = await execSync(startDocker).toString();
-//   } catch (e){
-//     startResult = e.stderr;
-//   }
-//   console.log('startResult', { startResult: startResult.toString() });
-
-//   while (startResult.indexOf('port is already arllocated') !== -1) {
-//     // fix hash that this port is busy
-//     portsHash[port] = 'broken';
-//     dockerPort = await getPort();
-//     portsHash[dockerPort] = containerName;
-//     const RestartDocker = `docker run -e PORT=${dockerPort} -e GQL_URN=${gqlURN} -e GQL_SSL=0 --name ${containerName} ${docker ? `--expose ${dockerPort}` : `-p ${dockerPort}:${dockerPort}` } --net ${network} -d ${handler} && npx wait-on http://${docker ? containerName : 'localhost'}:${dockerPort}/healthz`;
-//     console.log('already arllocated, RestartDocker', { RestartDocker });
-//     const startResult = await execSync(RestartDocker).toString();
-//     console.log('RestartResult', { startResult });
-//   }
-//   if (startResult.indexOf('is already in use by container') !== -1){
-//     const RestartDocker = `docker stop ${containerName} && docker rm ${containerName} && docker run -e PORT=${dockerPort} -e GQL_URN=${gqlURN} -e GQL_SSL=0 --name ${containerName} ${docker ? `--expose ${dockerPort}` : `-p ${dockerPort}:${dockerPort}` } --net ${network} -d ${handler} && npx wait-on http://${docker ? containerName : 'localhost'}:${dockerPort}/healthz`;
-//     console.log('already in use, RestartDocker', { RestartDocker });
-//     try {
-//       await execSync(RestartDocker).toString();
-//       console.log('RestartResult', { startResult: startResult.toString() });
-//     } catch (e){
-//       console.log('error', e);
-//       return ({ error: e });
-//     }
-//   }
-//   return await this.initHandler({...options, port: dockerPort});
-// }
-
 export async function handleOperation(operation: keyof typeof handlerOperations, oldLink: any, newLink: any) {
   const current = newLink ?? oldLink;
   const currentLinkId = current.id;
