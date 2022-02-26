8ba773efdec0789c1c9d4f4537b75667a6e7ed2f
Konstantin Dyachenko <drakonard@gmail.com>
2022-02-26 15:56:05 +0000

    Switched to selector handler test. https://github.com/deep-foundation/deeplinks/issues/7

--- tests/handlers.ts
+++ tests/handlers.ts
@@ -552,7 +552,7 @@ export async function insertSelectorItem({ selectorId, nodeTypeId, linkTypeId, t
   assert.lengthOf(n1?.data, 1, `item_id ${id2} must be in selector_id ${selectorId}`);
 };
 
-describe.skip('handle by selector', () => {
+describe.only('handle by selector', () => {
   it(`handle insert`, async () => {
     const numberToReturn = randomInteger(5000000, 9999999999);
     const handleSelectorTypeId = await deep.id('@deep-foundation/core', 'HandleSelector');
