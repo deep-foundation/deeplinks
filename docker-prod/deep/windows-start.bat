7cfe5faf198c7bed52fbbcd6f3e78c61114905bc
menzorg <menzorg@gmail.com>
2022-03-22 02:55:54 +0300

    windows starter alpha

--- docker-prod/deep/windows-start.bat
+++ docker-prod/deep/windows-start.bat
@@ -1,1 +1,3 @@
+wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml" -OutFile ".\docker-compose.yml"
+wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html" -OutFile ".\open-deep.html"
 docker-compose -p deep up -d
