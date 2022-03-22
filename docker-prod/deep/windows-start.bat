889e139a1b8a3ad05acade0972141de930e3bc67
menzorg <menzorg@gmail.com>
2022-03-22 03:03:53 +0300

    add migrations

--- docker-prod/deep/windows-start.bat
+++ docker-prod/deep/windows-start.bat
@@ -1,3 +1,4 @@
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml" -OutFile ".\docker-compose.yml"
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html" -OutFile ".\open-deep.html"
 docker-compose -p deep up -d
+$postParams = @{operation='run'}; wget -Method POST -Uri "http://localhost:3007/api/deeplinks" -Body $postParams
