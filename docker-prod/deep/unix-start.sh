a1086eb475c3448d989bf3a6e8db4c765a6d8b8c
menzorg <menzorg@gmail.com>
2022-03-22 05:40:40 +0300

    wait some time for docker container ready

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -1,7 +1,9 @@
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html > ./open-deep.html
 docker-compose -p deep up -d
-echo 'Migrating data. It will be faster soon, but now please wait...'
-curl -X POST http://localhost:3007/api/deeplinks  --data '{"operation":"run"}' --header "Content-Type: application/json"
+echo 'Migrating data.'
+sleep 8
+echo 'It will be faster soon, but now please wait...'
+curl -X POST http://localhost:3007/api/deeplinks --data '{"operation":"run"}' --header "Content-Type: application/json"
 echo 'Migrations done!'
 sleep 1
