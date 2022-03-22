8734fca53cd34ac35c10a334275d0a72254054c9
menzorg <menzorg@gmail.com>
2022-03-22 20:44:48 +0300

    fix

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -1,12 +1,12 @@
-curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
+curl https://github.com/deep-foundation/deeplinks/blob/deepcase-23-menzorg/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html > ./open-deep.html
 echo 'Starting containers'
-COMPOSE="$(docker-compose version --short)" MAJOR=${COMPOSE:0:1}; if [ "$MAJOR" = "2" ];  then export COMPATIBILITY='--compatibility'; fi; docker-compose -p $COMPATIBILITY deep up -d
+COMPOSE="$(docker-compose version --short)" MAJOR=${COMPOSE:0:1}; if [ "$MAJOR" = "2" ];  then export COMPATIBILITY='--compatibility'; fi; docker-compose -p deep $COMPATIBILITY up -d
+sleep 10
 echo 'Cleaning data'
 curl -s -X POST http://localhost:3007/api/deeplinks --data '{"operation":"reset"}' --header "Content-Type: application/json"
 echo 'Migrating data'
-sleep 8
-echo "\nIt will be faster soon, but now please wait..."
+echo 'It will be faster soon, but now please wait...'
 curl -s -X POST http://localhost:3007/api/deeplinks --data '{"operation":"run"}' --header "Content-Type: application/json"
 echo 'Migrations done! Please, open file "open-deep.html"'
 sleep 1
