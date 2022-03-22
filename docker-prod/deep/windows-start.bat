95758afc983e7ffdc5c7cf607d42c1c20bf231e7
menzorg <menzorg@gmail.com>
2022-03-22 03:29:51 +0300

    final alpha windows starter

--- docker-prod/deep/windows-start.bat
+++ docker-prod/deep/windows-start.bat
@@ -1,4 +1,5 @@
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml" -OutFile ".\docker-compose.yml"
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html" -OutFile ".\open-deep.html"
 docker-compose -p deep up -d
-$postParams = @{operation='run'}; wget -Method POST -Uri "http://localhost:3007/api/deeplinks" -Body $postParams
+$postParams = @{operation='run'}; wget -Method POST -Uri "http://localhost:3007/api/deeplinks" -Body $postParams | Out-Null
+echo 'migrations done'
