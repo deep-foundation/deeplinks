5b8ca9225890239cf8c6a37c29a8e08f92a115eb
menzorg <menzorg@gmail.com>
2022-03-22 05:23:02 +0300

    no beauty, only work

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -2,6 +2,6 @@ curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-pro
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html > ./open-deep.html
 docker-compose -p deep up -d
 echo 'Migrating data. It will be faster soon, but now please wait...'
-curl -s -X POST http://localhost:3007/api/deeplinks  --data '{"operation":"run"}' --header "Content-Type: application/json" > /dev/null
+curl -X POST http://localhost:3007/api/deeplinks  --data '{"operation":"run"}' --header "Content-Type: application/json"
 echo 'Migrations done!'
 sleep 1
