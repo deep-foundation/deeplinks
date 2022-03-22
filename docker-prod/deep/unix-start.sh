048b4067abb2fd12e98fc42e59a7374e6ec73d1c
menzorg <menzorg@gmail.com>
2022-03-22 04:52:34 +0300

    unix-auto-migrating

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -1,1 +1,7 @@
+curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
+curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html > ./open-deep.html
 docker-compose -p deep up -d
+echo 'Migrating data. It will be faster soon, but now please wait...'
+curl -s -X POST http://localhost:3007/api/deeplinks  --data '{"operation":"run"}' --header "Content-Type: application/json" > /dev/null
+echo 'Migrations done!'
+sleep 1
