375381848456974fb0dc70b1ab519792aa31e52f
menzorg <menzorg@gmail.com>
2022-03-22 04:27:48 +0300

    b fix 2

--- docker-prod/deep/windows-start.ps1
+++ docker-prod/deep/windows-start.ps1
@@ -1,7 +1,7 @@
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml" -OutFile ".\docker-compose.yml"
 wget -Uri "https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html" -OutFile ".\open-deep.html"
 docker-compose -p deep up -d
-echo 'Migrating data, please wait...'
+echo 'Migrating data. It will be faster soon, but now please wait...'
 $postParams = @{operation='run'}; wget -Method POST -Uri "http://localhost:3007/api/deeplinks" -Body $postParams | Out-Null
 echo 'Migrations done!'
 sleep 1
