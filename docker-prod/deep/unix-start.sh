c6e7ab47e25a0cc0d1c69b57311fa23172637a7f
menzorg <menzorg@gmail.com>
2022-03-25 02:22:10 +0300

    remove dev deps and use main

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -1,5 +1,5 @@
-curl https://raw.githubusercontent.com/deep-foundation/deeplinks/deepcase-23-menzorg/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
-curl https://raw.githubusercontent.com/deep-foundation/deeplinks/deepcase-23-menzorg/docker-prod/deep/open-deep.html > ./open-deep.html
+curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
+curl https://raw.githubusercontent.com/deep-foundation/deeplinks/main/docker-prod/deep/open-deep.html > ./open-deep.html
 echo 'Clean and starting containers'
 COMPOSE="$(docker-compose version --short)" MAJOR=${COMPOSE:0:1}; if [ "$MAJOR" = "2" ];  then export COMPATIBILITY='--compatibility'; fi; docker-compose -p deep $COMPATIBILITY down -v --remove-orphans && docker-compose pull && docker-compose -p deep $COMPATIBILITY up -d
 sleep 5
