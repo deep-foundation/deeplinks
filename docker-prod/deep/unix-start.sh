bb76f42f2cc66fe8f3600eebcbc413750c70970c
menzorg <menzorg@gmail.com>
2022-03-25 01:49:08 +0300

    do pull manually in docker-prod

--- docker-prod/deep/unix-start.sh
+++ docker-prod/deep/unix-start.sh
@@ -1,7 +1,7 @@
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/deepcase-23-menzorg/docker-prod/deep/docker-compose.yml > ./docker-compose.yml
 curl https://raw.githubusercontent.com/deep-foundation/deeplinks/deepcase-23-menzorg/docker-prod/deep/open-deep.html > ./open-deep.html
 echo 'Clean and starting containers'
-COMPOSE="$(docker-compose version --short)" MAJOR=${COMPOSE:0:1}; if [ "$MAJOR" = "2" ];  then export COMPATIBILITY='--compatibility'; fi; docker-compose -p deep $COMPATIBILITY down -v --remove-orphans && docker-compose -p deep $COMPATIBILITY up -d
+COMPOSE="$(docker-compose version --short)" MAJOR=${COMPOSE:0:1}; if [ "$MAJOR" = "2" ];  then export COMPATIBILITY='--compatibility'; fi; docker-compose -p deep $COMPATIBILITY down -v --remove-orphans && docker-compose pull && docker-compose -p deep $COMPATIBILITY up -d
 sleep 5
 echo 'Migrating data'
 sleep 5
