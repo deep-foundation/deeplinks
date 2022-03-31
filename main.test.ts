db47dc681ebb407ad53d57a7a0c72e1550db9372
Ivan S Glazunov <ivansglazunov@gmail.com>
2022-03-31 02:03:35 +0000

    fix mp tree typing

--- main.test.ts
+++ main.test.ts
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-// import './tests/client';
-// import './tests/join-insert';
-// import './tests/typing';
-// import './tests/selectors';
-// import './tests/bool_exp';
-// import './tests/permissions';
-// import './tests/handlers';
+import './tests/client';
+import './tests/join-insert';
+import './tests/typing';
+import './tests/selectors';
+import './tests/bool_exp';
+import './tests/permissions';
+import './tests/handlers';
 import './tests/minilinks';
-// import './tests/demo';
-// import './tests/packager';
+import './tests/demo';
+import './tests/packager';
 // TODO error if duplicates
