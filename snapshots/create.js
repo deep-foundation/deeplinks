95ebeb93e8e1a0ad47cfe42eedee40336c4b14db
menzorg <menzorg@gmail.com>
2022-04-20 20:25:46 +0300

    snapshots

---
+++ snapshots/create.js
@@ -0,0 +1,18 @@
+const  { promisify } = require('util');
+const { exec } = require('child_process');
+const { promises: fs } = require('fs');
+const execP = promisify(exec);
+const path = require('path');
+const _deeplinks = path.normalize(`${__dirname}/..`);
+
+const create = async () => {
+  const files = await fs.readdir('./snapshots');
+  const snapshots = files.filter(file => /^-?[0-9]+$/.test(file));
+  const last = snapshots[snapshots.length - 1];
+  const time = (new Date()).getTime();
+  const { stdout, stderr } = await execP(`cd docker-prod/deep && docker-compose -f docker-compose.yml stop graphql-engine postgres && docker run -v ${_deeplinks}:/deeplinks --volumes-from deep_postgres_1 --rm --name links --entrypoint \"sh\" deepf/deeplinks:main -c \"cd / && ls var/lib/postgresql && tar -c -v -C /var/lib/postgresql -f /deeplinks/snapshots/${time} ./data" && docker-compose -f docker-compose.yml start graphql-engine postgres && cd ../.. && cp .migrate snapshots/${time}.migrate`);
+  console.log('stdout',stdout);
+  console.log('stderr',stderr);
+}
+
+create();
