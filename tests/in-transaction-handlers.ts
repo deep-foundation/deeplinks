fdb0b1a6218eb4925a0e8eccf80a079d7dc55621
menzorg <menzorg@gmail.com>
2022-07-06 17:37:54 +0300

    new exec sync handlers

--- tests/in-transaction-handlers.ts
+++ tests/in-transaction-handlers.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import { generateApolloClient } from "@deep-foundation/hasura/client";
 import { DeepClient } from "../imports/client";
-import { insertHandler, insertSelector, insertSelectorItem, ensureLinkIsCreated, getPromiseResults, deletePromiseResult, deleteHandler, deleteSelector, deleteId } from './handlers'
+import { insertHandler, insertSelector, insertSelectorItem, ensureLinkIsCreated, deleteHandler, deleteSelector, deleteId, deleteIds }  from "../imports/handlers";
 import { HasuraApi } from'@deep-foundation/hasura/api';
 import { sql } from '@deep-foundation/hasura/sql';
 import Debug from 'debug';
@@ -37,61 +37,62 @@ const nextHandlerResult = () => {
   return lastHandlerLinkId;
 };
 
-describe.only('Deep client-mini', () => {
-  it(`id`, async () => {
-    const result = await api.sql(sql`select links__deep__client('id', '{"start": "@deep-foundation/core", "path":["RuleSubject"]}'::json)`);
-    const clientResult = await deep.id('@deep-foundation/core', 'RuleSubject');
-    assert.equal(result?.data?.result?.[1]?.[0], clientResult);
+describe('sync handlers', () => {
+  describe('DeepClient mini', () => {
+    it(`id`, async () => {
+      const result = await api.sql(sql`select links__deep__client('id', '{"start": "@deep-foundation/core", "path":["RuleSubject"]}'::json)`);
+      const clientResult = await deep.id('@deep-foundation/core', 'RuleSubject');
+      log(result?.data?.result?.[1]?.[0], clientResult);
+      assert.equal(result?.data?.result?.[1]?.[0], clientResult);
+    });
   });
-});
+  describe('Handle operations', () => {
+    it(`Handle insert`, async () => {
+      const CustomNumber = nextHandlerResult();
 
-describe('In-transaction handlers', () => {
-  it(`handle insert`, async () => {
-    const CustomNumber = nextHandlerResult();
+      const typeId = await deep.id('@deep-foundation/core', 'Type');
+      const handleInsertTypeId = await deep.id('@deep-foundation/core', 'HandleInsert');
+      const supportsId = await deep.id('@deep-foundation/core', 'plv8SupportsJs');
+      const handler = await insertHandler(
+        handleInsertTypeId,
+        typeId, 
+        `(deep, data) => { deep.insert({id: ${CustomNumber}, type_id: 2}); }`,
+        undefined,
+        supportsId
+      );
+      log('handler', handler);
+      log('CustomNumber', CustomNumber);
 
-    const typeId = await deep.id('@deep-foundation/core', 'Type');
-    const handleInsertTypeId = await deep.id('@deep-foundation/core', 'HandleInsert');
-    const supportsId = await deep.id('@deep-foundation/core', 'plv8SupportsJs');
-    const handler = await insertHandler(
-      handleInsertTypeId,
-      typeId, 
-      `deep.insert({id: ${CustomNumber}, type_id: 2});`,
-      undefined,
-      supportsId
-    );
-    log('handler', handler);
-    log('CustomNumber', CustomNumber);
+      const linkId = await ensureLinkIsCreated(typeId);
+      log('linkId', linkId);
+      const insertedByHandler = (await deep.select({ id: { _eq: CustomNumber } }));
+      log('insertedByHandler', insertedByHandler?.data[0]?.id);
+      if (insertedByHandler?.data[0]?.id) await deep.delete({ id: { _eq: CustomNumber } });
+      log('delete linkid', await deep.delete({ id: { _eq: linkId } }));
+      log('delete handler', await deleteHandler(handler));
+      assert.equal(insertedByHandler?.data[0]?.id, CustomNumber);
+    });
+    it.skip(`Handle insert on selector`, async () => {
+      const CustomNumber = nextHandlerResult();
 
-    const linkId = await ensureLinkIsCreated(typeId);
-    log('linkId', linkId);
-    const insertedByHandler = (await deep.select({ id: { _eq: CustomNumber } }));
-    log('insertedByHandler', insertedByHandler?.data[0]?.id);
-    if (insertedByHandler?.data[0]?.id) await deep.delete({ id: { _eq: CustomNumber } });
-    log('delete linkid', await deep.delete({ id: { _eq: linkId } }));
-    log('delete handler', await deleteHandler(handler));
-    assert(insertedByHandler?.data[0]?.id === CustomNumber);
-  });
-  it(`handle insert on selector`, async () => {
-    // const numberToReturn = randomInteger(5000000, 9999999999);
-    const CustomNumber = nextHandlerResult();
+      const handleInsertTypeId = await deep.id('@deep-foundation/core', 'HandleInsert');
+      const selector = await insertSelector();
+      const { nodeTypeId, linkTypeId, treeId, selectorId, rootId } = selector;
+      const handler = await insertHandler(
+        handleInsertTypeId,
+        selectorId,
+        `(arg) => {
+          plv8.execute("INSERT INTO strings (link_id, value) VALUES(${rootId}, 'test')");
+        }`);
+      const selectorItems = await insertSelectorItem({ selectorId, nodeTypeId, linkTypeId, treeId, rootId });
+      log('selectorItems', selectorItems)
 
-    const handleInsertTypeId = await deep.id('@deep-foundation/core', 'HandleInsert');
-    const selector = await insertSelector();
-    const { nodeTypeId, linkTypeId, treeId, selectorId, rootId } = selector;
-    const handler = await insertHandler(
-      handleInsertTypeId,
-      selectorId,
-      `(arg) => {
-        plv8.execute("INSERT INTO strings (link_id, value) VALUES(${rootId}, 'test')");
-      }`);
-    const selectorItems = await insertSelectorItem({ selectorId, nodeTypeId, linkTypeId, treeId, rootId });
-    log('selectorItems', selectorItems)
-
-    for (const selectorItem of selectorItems) {
-      await deleteId(selectorItem.linkId);
-      await deleteId(selectorItem.nodeId);
-    }
-    await deleteSelector(selector);
-    await deleteHandler(handler);
+      for (const selectorItem of selectorItems) {
+        await deleteId(selectorItem.linkId);
+        await deleteId(selectorItem.nodeId);
+      }
+      await deleteSelector(selector);
+      await deleteHandler(handler);
+    });
   });
 });
