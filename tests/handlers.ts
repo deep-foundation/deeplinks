562d624ef82621525dc2af5c48c6acc3c282f172
Konstantin Dyachenko <drakonard@gmail.com>
2022-07-30 12:52:59 +0000

    Attempt to reproduce problem.

--- tests/handlers.ts
+++ tests/handlers.ts
@@ -347,7 +347,7 @@ afterAll(async () => {
   deletePackageWithPermissions(packageWithPermissions);
 });
 
-describe.only('sync function handle by type with resolve', () => {
+describe('sync function handle by type with resolve', () => {
   it(`handle insert`, async () => {
     // const numberToReturn = randomInteger(5000000, 9999999999);
     const numberToReturn = nextHandlerResult();
@@ -470,8 +470,8 @@ describe.only('sync function handle by type with resolve', () => {
   });
 });
 
-describe.only('sync function handle by type with reject', () => {
-  it(`handle insert`, async () => {
+describe('sync function handle by type with reject', () => {
+  it.only(`handle insert`, async () => {
     // const numberToThrow = randomInteger(5000000, 9999999999);
     const numberToThrow = nextHandlerResult();
 
