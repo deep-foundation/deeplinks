5bc8b1ff6375e7babb48b29371a468e576a30593
Menzorg <menzorg@gmail.com>
2022-08-02 23:43:32 +0300

    snapshot:last fix

--- snapshots/last.js
+++ snapshots/last.js
@@ -9,7 +9,7 @@ const last = async () => {
   const files = await fs.readdir('./snapshots');
   const snapshots = files.filter(file => /^-?[0-9]+$/.test(file));
   const last = snapshots[snapshots.length - 1]
-  const { stdout, stderr } = await execP(`cd docker-prod/deep && (docker-compose -f docker-compose.yml stop graphql-engine postgres || true ) && docker run -v ${_deeplinks}:/deeplinks -v deep_db_data:/data --rm --name links --entrypoint \"sh\" deepf/deeplinks:main -c \"cd / && tar xf deeplinks/snapshots/${last} --strip 1\" && docker-compose -f docker-compose.yml start graphql-engine postgres && cd ../.. && cp snapshots/${last}.migrate .migrate`);
+  const { stdout, stderr } = await execP(`cd docker-prod/deep && (docker-compose -f docker-compose.yml stop graphql-engine postgres || true ) && docker run -v ${_deeplinks}:/deeplinks -v deep_db_data:/data --rm --name links --entrypoint \"sh\" deepf/deeplinks:main -c \"cd / && tar xf deeplinks/snapshots/${last} --strip 1\" && (docker-compose -f docker-compose.yml start graphql-engine postgres || true) && cd ../.. && cp snapshots/${last}.migrate .migrate`);
   console.log('stdout',stdout);
   console.log('stderr',stderr);
 }
