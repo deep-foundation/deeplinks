37a83e0f3e4907ee706e0efa6a25ed7bb41853b4
Ivan S Glazunov <ivansglazunov@gmail.com>
2022-08-05 23:08:29 +0300

    https://github.com/deep-foundation/deeplinks/issues/74

--- migrations/1622421760259-can.ts
+++ migrations/1622421760259-can.ts
@@ -12,6 +12,8 @@ const error = debug.extend('error');
 
 export const CAN_TABLE_NAME = 'can';
 export const TABLE_NAME = 'links';
+export const CACHE = 'selectors_cache';
+export const MP = 'mp';
 
 const client = generateApolloClient({
   path: `${process.env.MIGRATIONS_HASURA_PATH}/v1/graphql`,
@@ -28,30 +30,76 @@ export const up = async () => {
   log('view');
   await api.sql(sql`
     CREATE VIEW ${CAN_TABLE_NAME} AS
-    SELECT DISTINCT r."id" as "rule_id", sr_o."item_id" as "object_id", sr_s."item_id" as "subject_id", sr_a."item_id" as "action_id"
+    SELECT DISTINCT ca."rule_id" as "rule_id", mpo_include."item_id" as "object_id", mps_include."item_id" as "subject_id", mpa_include."item_id" as "action_id"
     FROM
-    ${TABLE_NAME} as r,
-    ${TABLE_NAME} as o,
-    ${SELECTORS_TABLE_NAME} as sr_o,
-    ${TABLE_NAME} as sr_o_ex_up, -- ANY OR REALY IDS
-    ${TABLE_NAME} as s,
-    ${SELECTORS_TABLE_NAME} as sr_s,
-    ${TABLE_NAME} as a,
-    ${SELECTORS_TABLE_NAME} as sr_a
-    WHERE
-    r."type_id" = ${await deep.id('@deep-foundation/core', 'Rule')} AND
+    ${CACHE} as ca,
+    ${MP} as mpa_include,
+    ${CACHE} as co,
+    ${MP} as mpo_include,
+    ${CACHE} as cs,
+    ${MP} as mps_include
+    WHERE (
+      ca."rule_id" != 0 AND
+      co."rule_id" != 0 AND
+      cs."rule_id" != 0 AND
 
-    o."type_id" = ${await deep.id('@deep-foundation/core', 'RuleObject')} AND
-    o."from_id" = r."id" AND
-    (o."to_id" = sr_o."selector_id" OR o."to_id" = ${await deep.id('@deep-foundation/core', 'Any')}) AND
+      ca."rule_id" = co."rule_id" AND
+      ca."rule_id" = cs."rule_id" AND
 
-    s."type_id" = ${await deep.id('@deep-foundation/core', 'RuleSubject')} AND
-    s."from_id" = r."id" AND
-    (s."to_id" = sr_s."selector_id" OR s."to_id" = ${await deep.id('@deep-foundation/core', 'Any')}) AND
+      ca."rule_action_id" != 0 AND
+      ca."selector_include_id" != 0 AND
+      mpa_include."path_item_id" = ca."link_id" AND
+      mpa_include."group_id" = ca."tree_id" AND
+      NOT EXISTS (
+        SELECT mpa_exclude."id"
+        FROM
+        ${CACHE} as cache_exclude,
+        ${MP} as mpa_exclude
+        WHERE (
+          cache_exclude."selector_exclude_id" != 0 AND
+          cache_exclude."selector_id" = ca."selector_id" AND
+          mpa_exclude."path_item_id" = cache_exclude."link_id" AND
+          mpa_exclude."item_id" = mpa_include."item_id" AND
+          mpa_exclude."group_id" = cache_exclude."tree_id"
+        )
+      ) AND
 
-    a."type_id" = ${await deep.id('@deep-foundation/core', 'RuleAction')} AND
-    a."from_id" = r."id" AND
-    (a."to_id" = sr_a."selector_id" OR a."to_id" = ${await deep.id('@deep-foundation/core', 'Any')})
+      co."rule_object_id" != 0 AND
+      co."selector_include_id" != 0 AND
+      mpo_include."path_item_id" = co."link_id" AND
+      mpo_include."group_id" = co."tree_id" AND
+      NOT EXISTS (
+        SELECT mpo_exclude."id"
+        FROM
+        ${CACHE} as cache_exclude,
+        ${MP} as mpo_exclude
+        WHERE (
+          cache_exclude."selector_exclude_id" != 0 AND
+          cache_exclude."selector_id" = co."selector_id" AND
+          mpo_exclude."path_item_id" = cache_exclude."link_id" AND
+          mpo_exclude."item_id" = mpo_include."item_id" AND
+          mpo_exclude."group_id" = cache_exclude."tree_id"
+        )
+      ) AND
+
+      cs."rule_subject_id" != 0 AND
+      cs."selector_include_id" != 0 AND
+      mps_include."path_item_id" = cs."link_id" AND
+      mps_include."group_id" = cs."tree_id" AND
+      NOT EXISTS (
+        SELECT mps_exclude."id"
+        FROM
+        ${CACHE} as cache_exclude,
+        ${MP} as mps_exclude
+        WHERE (
+          cache_exclude."selector_exclude_id" != 0 AND
+          cache_exclude."selector_id" = cs."selector_id" AND
+          mps_exclude."path_item_id" = cache_exclude."link_id" AND
+          mps_exclude."item_id" = mps_include."item_id" AND
+          mps_exclude."group_id" = cache_exclude."tree_id"
+        )
+      )
+    );
   `);
   await api.query({
     type: 'track_table',
