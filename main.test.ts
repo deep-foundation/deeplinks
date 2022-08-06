c5b02dfecbe2e05a1220aa19d879b6ff87881241
menzorg <menzorg@gmail.com>
2022-08-06 18:45:06 +0300

    clean main.test.ts

--- main.test.ts
+++ main.test.ts
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 jest.setTimeout(1000000);
 
-// import './tests/client';
-// import './tests/join-insert';
-// import './tests/typing';
-// import './tests/selectors';
-// import './tests/bool_exp';
-// import './tests/permissions';
-// import './tests/handlers';
-// import './tests/minilinks';
-// import './tests/demo';
-// import './tests/packager';
+import './tests/client';
+import './tests/join-insert';
+import './tests/typing';
+import './tests/selectors';
+import './tests/bool_exp';
+import './tests/permissions';
+import './tests/handlers';
+import './tests/minilinks';
+import './tests/demo';
+import './tests/packager';
 import './tests/sync-handlers';
 // import './tests/messanger';
 
