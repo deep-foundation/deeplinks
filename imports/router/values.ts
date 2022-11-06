401cc9086d4460cfed921351a8ca5ef13b00c286
Konstantin Dyachenko <drakonard@gmail.com>
2022-11-06 18:15:38 +0000

    try catch zone is bigger now

--- imports/router/values.ts
+++ imports/router/values.ts
@@ -51,94 +51,100 @@ export default async (req, res) => {
     const event = req?.body?.event;
     const operation = event?.op;
     if (operation === 'INSERT' || operation === 'UPDATE' || operation === 'DELETE') {
-      let oldValueRow = event?.data?.old;
-      let newValueRow = event?.data?.new;
-
-      const linkId = newValueRow?.link_id ?? oldValueRow?.link_id;
-      const linkRow = (await deep.select({
-        id: { _eq: linkId },
-      }, {
-        returning: `id from_id type_id to_id`,
-      }))?.data?.[0];
-
-      // get oldLinkRow and newLinkRow from promise_links table
-      // await api.sql(sql`CREATE TABLE IF NOT EXISTS public.promise_links (
-      //   id bigserial PRIMARY KEY,
-      //   promise_id bigint NOT NULL,
-      //   old_link_id bigint,
-      //   old_link_type_id bigint,
-      //   old_link_from_id bigint,
-      //   old_link_to_id bigint,
-      //   new_link_id bigint,
-      //   new_link_type_id bigint,
-      //   new_link_from_id bigint,
-      //   new_link_to_id bigint,
-      //   handle_operation_id bigint NOT NULL
-      // );`);
-      const handleUpdateTypeId = await deep.id('@deep-foundation/core', 'HandleUpdate');
-      /*
-      const queryResult = (await deep.select({
-        handle_operation_id: { _eq: handleUpdateTypeId },
-      }, {
-        returning: `
-          id
-          promise_id
-          old_link_id
-          old_link_type_id
-          old_link_from_id
-          old_link_to_id
-          new_link_id
-          new_link_type_id
-          new_link_from_id
-          new_link_to_id
-          handle_operation_id
-        `,
-      }))?.data?.[0];
-      */
-      // rewrite this query using apollo client
-      const queryResult = (await client.query({
-        query: gql`
-          query {
-            promise_links(where: {
-              handle_operation: { type_id: { _eq: ${handleUpdateTypeId} } }
-              old_link_id: {_eq: ${linkId}},
-              new_link_id: {_eq: ${linkId}},
-              values_operation: { _eq: "${operation}" }
-            }) {
-              id
-              promise_id
-              old_link_id
-              old_link_type_id
-              old_link_from_id
-              old_link_to_id
-              new_link_id
-              new_link_type_id
-              new_link_from_id
-              new_link_to_id
-              handle_operation_id
-            }
-          }
-        `,
-      }))?.data?.promise_links?.[0];
-      const oldLinkRow = {
-        id: queryResult?.old_link_id,
-        type_id: queryResult?.old_link_type_id,
-        from_id: queryResult?.old_link_from_id,
-        to_id: queryResult?.old_link_to_id,
-      };
-      const newLinkRow = {
-        id: queryResult?.new_link_id,
-        type_id: queryResult?.new_link_type_id,
-        from_id: queryResult?.new_link_from_id,
-        to_id: queryResult?.new_link_to_id,
-      };
-      log(`oldLinkRow: ${JSON.stringify(oldLinkRow)}`);
-      log(`newLinkRow: ${JSON.stringify(newLinkRow)}`);
-
-      const oldRow = { ...linkRow, value: oldValueRow };
-      const newRow = { ...linkRow, value: newValueRow };
-
+      let linkId;
+      let linkRow;
+      let oldValueRow;
+      let newValueRow;
+      let newRow;
+      let oldRow;
       try {
+        oldValueRow = event?.data?.old;
+        newValueRow = event?.data?.new;
+
+        linkId = newValueRow?.link_id ?? oldValueRow?.link_id;
+        linkRow = (await deep.select({
+          id: { _eq: linkId },
+        }, {
+          returning: `id from_id type_id to_id`,
+        }))?.data?.[0];
+
+        // get oldLinkRow and newLinkRow from promise_links table
+        // await api.sql(sql`CREATE TABLE IF NOT EXISTS public.promise_links (
+        //   id bigserial PRIMARY KEY,
+        //   promise_id bigint NOT NULL,
+        //   old_link_id bigint,
+        //   old_link_type_id bigint,
+        //   old_link_from_id bigint,
+        //   old_link_to_id bigint,
+        //   new_link_id bigint,
+        //   new_link_type_id bigint,
+        //   new_link_from_id bigint,
+        //   new_link_to_id bigint,
+        //   handle_operation_id bigint NOT NULL
+        // );`);
+        const handleUpdateTypeId = await deep.id('@deep-foundation/core', 'HandleUpdate');
+        /*
+        const queryResult = (await deep.select({
+          handle_operation_id: { _eq: handleUpdateTypeId },
+        }, {
+          returning: `
+            id
+            promise_id
+            old_link_id
+            old_link_type_id
+            old_link_from_id
+            old_link_to_id
+            new_link_id
+            new_link_type_id
+            new_link_from_id
+            new_link_to_id
+            handle_operation_id
+          `,
+        }))?.data?.[0];
+        */
+        // rewrite this query using apollo client
+        const queryResult = (await client.query({
+          query: gql`
+            query {
+              promise_links(where: {
+                handle_operation: { type_id: { _eq: ${handleUpdateTypeId} } }
+                old_link_id: {_eq: ${linkId}},
+                new_link_id: {_eq: ${linkId}},
+                values_operation: { _eq: "${operation}" }
+              }) {
+                id
+                promise_id
+                old_link_id
+                old_link_type_id
+                old_link_from_id
+                old_link_to_id
+                new_link_id
+                new_link_type_id
+                new_link_from_id
+                new_link_to_id
+                handle_operation_id
+              }
+            }
+          `,
+        }))?.data?.promise_links?.[0];
+        const oldLinkRow = {
+          id: queryResult?.old_link_id,
+          type_id: queryResult?.old_link_type_id,
+          from_id: queryResult?.old_link_from_id,
+          to_id: queryResult?.old_link_to_id,
+        };
+        const newLinkRow = {
+          id: queryResult?.new_link_id,
+          type_id: queryResult?.new_link_type_id,
+          from_id: queryResult?.new_link_from_id,
+          to_id: queryResult?.new_link_to_id,
+        };
+        log(`oldLinkRow: ${JSON.stringify(oldLinkRow)}`);
+        log(`newLinkRow: ${JSON.stringify(newLinkRow)}`);
+
+        oldRow = { ...linkRow, value: oldValueRow };
+        newRow = { ...linkRow, value: newValueRow };
+
         if (!linkRow) {
           if(newValueRow?.link_id) {
             throw new Error('Value insert is handled before the link is added.'); 
