5cedb4060d6a40f5b26727b9a2e88b6f1d261cdb
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-13 08:22:10 +0000

    Migrate to apollo server v4

--- tests/handlers.ts
+++ tests/handlers.ts
@@ -1094,7 +1094,7 @@ describe('Async handlers', () => {
                           const express = require('express');
                           const http = require('http');
                           const ApolloServer = require('@apollo/server').ApolloServer;
-                          const { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+                          const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
 
                           const typeDefs = 'type Query { ${field}: Int }';
 
