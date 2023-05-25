30ba8e86b75279cc89d89364be0432278215cdd7
menzorg <menzorg@gmail.com>
2023-05-26 01:05:03 +0400

    snapshots fix

--- snapshots/last.js
+++ snapshots/last.js
@@ -9,7 +9,7 @@ const last = async () => {
   const files = await fs.readdir('./snapshots');
   const snapshots = files.filter(file => /^-?[0-9]+$/.test(file));
   const last = snapshots[snapshots.length - 1];
-  const { stdout, stderr } = await execP(`cd docker-prod/deep && (docker-compose -f docker-compose.yml stop hasura postgres || true ) && docker run -v ${_deeplinks}:/deeplinks -v deep-db-data:/data --rm --name links --entrypoint \"sh\" deepf/deeplinks:main -c \"cd / && tar xf deeplinks/snapshots/${last} --strip 1\" && (docker-compose -f docker-compose.yml start hasura postgres || true) && cd ../.. && cp snapshots/${last}.migrate .migrate`);
+  const { stdout, stderr } = await execP(`cd docker-prod/deep && (docker-compose -p deep -f docker-compose.yml stop hasura postgres || true ) && docker run -v ${_deeplinks}:/deeplinks -v deep-db-data:/data --rm --name links --entrypoint \"sh\" deepf/deeplinks:main -c \"cd / && tar xf deeplinks/snapshots/${last} --strip 1\" && (docker-compose -p deep up -d hasura postgres || true) && cd ../.. && cp snapshots/${last}.migrate .migrate`);
   console.log('stdout',stdout);
   console.log('stderr',stderr);
 }
