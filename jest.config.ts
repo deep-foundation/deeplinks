fc9b9796ad0d9eece09f4927c70fe08fd8bc23a2
Ivan S Glazunov <ivansglazunov@gmail.com>
2023-05-25 20:33:08 +0000

    0.0.165 ncu -u update and manual fixes for upgrade dependencies. Almoust all tests works.

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
+  // testEnvironmentOptions: {
+  //   url: "http://localhost/",
+  // },
+  verbose: true,
 };
