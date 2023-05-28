997a495daf02308f90e5712e7f61fadb8eea354b
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-28 21:26:20 +0000

    Attempt to fix complier.

--- compiler/index.js
+++ compiler/index.js
@@ -1,3 +1,8 @@
-require('ts-node').register(require('../tsconfig.json'));
-require('dotenv').config();
-module.exports = () => {};
+import tsNode from 'ts-node';
+import config from '../tsconfig.json';
+import dotenv from 'dotenv';
+
+tsNode.register(config);
+dotenv.config();
+
+export default () => {};
