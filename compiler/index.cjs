18caf47fb7e3612c55d84c62aaede3165ef95fcd
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-28 21:34:17 +0000

    Another attempt to fix migrations.

---
+++ compiler/index.cjs
@@ -0,0 +1,3 @@
+require('ts-node').register(require('../tsconfig.json'));
+require('dotenv').config();
+module.exports = () => {};
--- compiler/index.js
+++
@@ -1,8 +0,0 @@
-import tsNode from 'ts-node';
-import config from '../tsconfig.json';
-import dotenv from 'dotenv';
-
-tsNode.register(config);
-dotenv.config();
-
-export default () => {};
