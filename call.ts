0979ae3f4f71f3280926f6ddd016477d76de3481
Konstantin Dyachenko <drakonard@gmail.com>
2023-05-31 20:22:42 +0000

    import fix

--- call.ts
+++ call.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { call } from './imports/engine'
+import { call } from './imports/engine.js'
 
 const DEEPLINKS_CALL_OPTIONS = process.env.DEEPLINKS_CALL_OPTIONS || '{ "operation": "run" }';
 
