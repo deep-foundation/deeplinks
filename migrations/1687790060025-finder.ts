5e382d5b8d0811283a56f073f40679a8f26d2677
Ivan S Glazunov <ivansglazunov@gmail.com>
2023-06-26 20:48:28 +0000

    0.0.198

---
+++ migrations/1687790060025-finder.ts
@@ -0,0 +1,33 @@
+import { generateApolloClient } from '@deep-foundation/hasura/client.js';
+import Debug from 'debug';
+import { DeepClient } from '../imports/client.js';
+import npmPackagerPckg from '@deep-foundation/finder/deep.json' assert { type: 'json'};
+import { importPackage, sharePermissions } from './1664940577200-tsx.js';
+
+const debug = Debug('deeplinks:migrations:finder');
+const log = debug.extend('log');
+const error = debug.extend('error');
+
+const rootClient = generateApolloClient({
+  path: `${process.env.MIGRATIONS_HASURA_PATH}/v1/graphql`,
+  ssl: !!+(process.env.MIGRATIONS_HASURA_SSL || 0),
+  secret: process.env.MIGRATIONS_HASURA_SECRET,
+});
+
+const root = new DeepClient({
+  apolloClient: rootClient,
+});
+
+export const up = async () => {
+  log('up');
+  const importResult = await importPackage(npmPackagerPckg);
+  log(importResult);
+  const packageId = importResult?.packageId;
+  if (packageId) {
+    await sharePermissions(await root.id('deep', 'admin'), packageId);
+  }
+};
+
+export const down = async () => {
+  log('down');
+};
