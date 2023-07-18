5c45c1895df9f6defbb3ec450d7a6487ad3e792a
Konstantin Dyachenko <drakonard@gmail.com>
2023-07-18 21:04:18 +0000

    attempt to fix

--- call.cjs
+++ call.cjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { call } = require('./imports/engine.cjs');
+const engine = require('./imports/engine.cjs');
 
 const DEEPLINKS_CALL_OPTIONS = process.env.DEEPLINKS_CALL_OPTIONS || '{ "operation": "run" }';
 
 (async() => {
-  await call(JSON.parse(DEEPLINKS_CALL_OPTIONS));
+  await engine.call(JSON.parse(DEEPLINKS_CALL_OPTIONS));
 })()
