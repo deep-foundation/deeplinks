73e71385af02b12e9c74c48a9bf3949e4acabc2c
Konstantin Dyachenko <drakonard@gmail.com>
2023-07-31 17:27:55 +0000

    Better useTokenController

--- imports/react-token.tsx
+++ imports/react-token.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { TokenContext } from '@deep-foundation/react-hasura/token-context.js';
 import { useLocalStore } from '@deep-foundation/store/local.js';
+import { useCookiesStore } from '@deep-foundation/store/cookies.js'
 
 export function useTokenController() {
-  return useLocalStore('dc-dg-token', '');
+  const [localToken, setLocalToken] = useLocalStore('dc-dg-token', '');
+  const [cookieToken, setCookieToken] = useCookiesStore('dc-dg-token', '');
+
+  return [localToken || cookieToken, (newToken) => {
+    const result = setLocalToken(newToken);
+    setCookieToken(newToken);
+    return result;
+  }]
 }
 
 export function TokenProvider({ children }: { children?: any }) {
   const [token, setToken] = useTokenController();
-  // console.log('TokenProvider', { token });
+  console.log('TokenProvider', { token });
   // @ts-ignore
   return <TokenContext.Provider value={token}>{children}</TokenContext.Provider>;
 }
