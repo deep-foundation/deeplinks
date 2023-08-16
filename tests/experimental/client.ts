fe88d415b44fb0bca4eab75280c8b039a79c6f6f
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-16 19:15:17 +0600

    Add tests for SerialTransitionsBuilder

---
+++ tests/experimental/client.ts
@@ -0,0 +1,120 @@
+import { generateApolloClient } from '@deep-foundation/hasura/client.js';
+import { ApolloClient} from '@apollo/client/index.js';
+import '@testing-library/jest-dom';
+import { SerialTransitionsBuilder, Transition } from '../../imports/experimental/serial-transitions-builder';
+import { DeepClient, Table } from '../../imports/client';
+
+
+const graphQlPath = `${process.env.DEEPLINKS_HASURA_PATH}/v1/graphql`;
+const ssl = !!+process.env.DEEPLINKS_HASURA_SSL;
+const secret = process.env.DEEPLINKS_HASURA_SECRET;
+const ws = true;
+
+let apolloClient: ApolloClient<any>;
+let deepClient: DeepClient;
+
+beforeAll(async () => {
+  apolloClient = generateApolloClient({
+    path: graphQlPath,
+    ssl,
+    secret,
+    ws
+  });
+  deepClient = new DeepClient({ apolloClient });
+
+  const {data: corePackageLinks} = await deepClient.select({
+    up: {
+      tree_id: {
+        _id: ["@deep-foundation/core" ,"containTree"]
+      },
+      parent_id: {
+        _id: ["@deep-foundation/core"]
+      }
+    }
+  })
+  deepClient.minilinks.apply(corePackageLinks)
+})
+
+describe('SerialTransitionsBuilder', () => {
+  it('should instantiate correctly', () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    expect(builder).toBeInstanceOf(SerialTransitionsBuilder);
+  });
+
+  it('should append insert transition', async () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const table = 'links' as Table<'insert'>;
+    const transition = [null, {
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }];
+    builder.append({ table, transition });
+    expect(builder.actions().length).toBe(1);
+  });
+
+  it('should clear transitions', () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const table = 'links' as Table<'insert'>;
+    const transition = [null, {
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }];
+    builder.append({ table, transition });
+    builder.clear();
+    expect(builder.actions().length).toBe(0);
+  });
+
+  it('should get correct transition type for insert', () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const transition: Transition = [null, {
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }];
+    const type = builder.getTransitionType(transition); 
+    if(type !== 'insert') {
+      throw new Error('Incorrect transition type');
+    }
+  });
+
+  it('should get correct transition type for update', () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const transition: Transition = [{
+      id: 0
+    }, {
+      id: 0
+    }];
+    const type = builder.getTransitionType(transition); 
+    if(type !== 'update') {
+      throw new Error('Incorrect transition type');
+    }
+  });
+
+  it('should get correct transition type for delete', () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const transition: Transition = [{
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }, null];
+    const type = builder.getTransitionType(transition); 
+    if(type !== 'delete') {
+      throw new Error('Incorrect transition type');
+    }
+  });
+
+  it('should execute transition', async () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const transition = [null, {
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }];
+    await builder
+    .append({ transition })
+    .execute();
+  });
+
+  it('should execute transitions', async () => {
+    const builder = new SerialTransitionsBuilder({ deep: deepClient });
+    const transition = [null, {
+      type_id: deepClient.idLocal("@deep-foundation/core", "Type")
+    }];
+    await builder
+    .append({ transition })
+    .append({ transition })
+    .execute();
+  });
+});
