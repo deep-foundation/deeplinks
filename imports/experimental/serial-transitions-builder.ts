753744d3c9a3cd955009b5becdddabbe9f5f6fc5
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-16 18:33:31 +0600

    Updaet SerialTransitionsBuilder

--- imports/experimental/serial-transitions-builder.ts
+++ imports/experimental/serial-transitions-builder.ts
@@ -1,31 +1,50 @@
 import { AsyncSerialParams, DeepClient, InsertTable, OperationType, SerialOperation, Table } from "../client";
-import { createSerialOperation } from "../gql";
+import { createSerialOperation, deleteMutation, generateSerial, IGenerateMutationBuilder, insertMutation, ISerialOptions, updateMutation } from "../gql";
 import { Link } from "../minilinks";
 
-
-
 export class SerialTransitionsBuilder {
-  public deep: DeepClient;
+  private deep: DeepClient;
+  private serialActions: Array<IGenerateMutationBuilder>;
 
-    constructor(param) {
-        this.deep = param.deep;
+    constructor(options: SerialTransitionsBuilderOptions) {
+        this.deep = options.deep;
+        this.serialActions = new Array<IGenerateMutationBuilder>();
     }
 
-    public appendTransition(options: AppendTransitionOptions) {
+    public append(options: AppendTransitionOptions) {
       const {table,transition} = options;
       const transitionType = this.getTransitionType(transition)
-      let serialOperation: SerialOperation;
+      let serialAction: IGenerateMutationBuilder;
       switch (transitionType) {
         case 'insert':
-          serialOperation = createSerialOperation({
-            type: transitionType,
-            table: table,
-          })
+          serialAction = insertMutation(table, {objects: transition[1]})
           break;
-      
+        case 'update':
+          serialAction = updateMutation(table, {exp: transition[0], value: transition[1]})
+          break;
+        case 'delete':
+          serialAction = deleteMutation(table, {exp: transition[0]})
         default:
-          break;
+            throw new Error('Invalid transition type. If you want to insert link - the first element must be null and the second must be link. If you want to update link - the first and second elements must be links. If you want to delete link - the first element must be link and second must be null')
       }
+      this.serialActions.push(serialAction)
+      return this;
+    }
+
+    public clear() {
+      this.serialActions = new Array<IGenerateMutationBuilder>();
+      return this;
+    }
+
+    public async execute(options: ExecuteOptions) {
+      return await this.deep.apolloClient.mutate(generateSerial({
+        actions: this.serialActions,
+        ...options
+      }))
+    }
+
+    public async actions() {
+      return this.actions;
     }
 
     public getTransitionType(transition: Transition): TransitionType {
@@ -41,11 +60,17 @@ export class SerialTransitionsBuilder {
     }
 }
 
-export interface AppendTransitionOptions<TTransition extends Transition> {
-  transition: TTransition;
-  table: Table<TTransition[0] extends null ? 'insert' : TTransition[1] extends null ? 'delete' : 'update'>;
+export interface AppendTransitionOptions {
+  transition: Transition;
+  table: Table<Transition[0] extends null ? 'insert' : Transition[1] extends null ? 'delete' : 'update'>;
 }
 
-export type TransitionType = Exclude<OperationType, 'select'>;
+export type TransitionType = 'insert'|'update'|'delete';
 export type TransitionItem = Link<number> | null;
-export type Transition = Array<TransitionItem>
+export type Transition = Array<TransitionItem>;
+
+export type ExecuteOptions = Omit<ISerialOptions, 'actions'>
+
+export type SerialTransitionsBuilderOptions = {
+  deep: DeepClient;
+}
