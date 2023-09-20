d1143003c30d4f11793bcbd9797b17af43b86e0f
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-20 20:51:35 +0600

    Change error throw for serial

--- imports/client.tsx
+++ imports/client.tsx
@@ -744,7 +744,7 @@ export class DeepClient<L = Link<number>> implements DeepClientInstance<L> {
     } catch (e) {
       const sqlError = e?.graphQLErrors?.[0]?.extensions?.internal?.error;
       if (sqlError?.message) e.message = sqlError.message;
-      if (!silent) throw e;
+      if (!silent) throw new Error(`DeepClient Insert Error: ${e.message}`, { cause: e });
       return { ...result, data: (result)?.data?.m0?.returning, error: e };
     }
     // @ts-ignore
