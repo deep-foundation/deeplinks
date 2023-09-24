42d1406bf124334722b1bc5206a44e9847c9429b
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-24 14:08:34 +0600

    Add defaultValueparameter  to useTokenController

--- imports/react-token.tsx
+++ imports/react-token.tsx
@@ -3,9 +3,9 @@ import { TokenContext } from '@deep-foundation/react-hasura/token-context.js';
 import { useLocalStore } from '@deep-foundation/store/local.js';
 import { useCookiesStore } from '@deep-foundation/store/cookies.js'
 
-export function useTokenController() : [string, (string) => string] {
-  const [localToken, setLocalToken] = useLocalStore('dc-dg-token', '');
-  const [cookieToken, setCookieToken] = useCookiesStore('dc-dg-token', '');
+export function useTokenController(defaultValue: string = '') : [string, (string) => string] {
+  const [localToken, setLocalToken] = useLocalStore('dc-dg-token', defaultValue);
+  const [cookieToken, setCookieToken] = useCookiesStore('dc-dg-token', defaultValue);
 
   return [localToken || cookieToken, (newToken) => {
     const result = setLocalToken(newToken);
