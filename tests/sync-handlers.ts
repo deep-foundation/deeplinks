8539b75899ef5077fa5d303f29f0f83740af4baf
FreePhoenix888 <freephoenix888@gmail.com>
2023-11-18 15:58:33 +0000

    Update test select in handler

--- tests/sync-handlers.ts
+++ tests/sync-handlers.ts
@@ -3035,14 +3035,18 @@ describe('sync handlers', () => {
       type_id: await deep.id("@deep-foundation/core", "Type")
     }).then(result => result.data[0].id);
     const handleInsertTypeLinkId = await deep.id('@deep-foundation/core', 'HandleInsert');
+    const supportsId = await deep.id("@deep-foundation/core", "plv8SupportsJs")
     const handler = await insertHandler(handleInsertTypeLinkId, customTypeLinkId, 
       `(arg) => {
         const deepSelectResult = deep.select({
           id: ${customTypeLinkId}
         })
-        if(!deepSelectResult) {
+        if(deepSelectResult.data.length === 0) {
           throw new Error('Select in handler returns empty array');
         }
-      }`);
+      }`, undefined, supportsId);
+      await deep.insert({
+        type_id: customTypeLinkId
+      })
   })
 });
