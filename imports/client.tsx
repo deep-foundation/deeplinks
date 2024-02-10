435697789483157ddd6a9545fbb6f18619fc6676
FreePhoenix888 <freephoenix888@gmail.com>
2024-02-10 12:40:56 +0600

    Add example `Delete by to_id`

--- imports/client.tsx
+++ imports/client.tsx
@@ -1044,6 +1044,14 @@ export class DeepClient<L extends Link<number> = Link<number>> implements DeepCl
    * })
    * ```
    * In this case all the links with from_id 888 will be deleted
+   * 
+   * #### Delete by to_id
+   * ``` 
+   * await deep.delete({
+   *   to_id: 888
+   * })
+   * ```
+   * In this case all the links with to_id 888 will be deleted
    */
   async delete<TTable extends 'links'|'numbers'|'strings'|'objects'>(exp: Exp<TTable>, options?: WriteOptions<TTable>):Promise<DeepClientResult<{ id }[]>> {
     if (!exp) throw new Error('!exp');
