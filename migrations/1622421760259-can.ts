5f2b9ae358dfc21e3bb87bfde00480d9c5886d00
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-11 06:14:44 +0300

    0.0.491

--- migrations/1622421760259-can.ts
+++ migrations/1622421760259-can.ts
@@ -32,74 +32,22 @@ export const up = async () => {
     CREATE VIEW ${CAN_TABLE_NAME} AS
     SELECT DISTINCT ca."rule_id" as "rule_id", mpo_include."item_id" as "object_id", mps_include."item_id" as "subject_id", mpa_include."item_id" as "action_id"
     FROM
-    ${CACHE} as ca,
-    ${MP} as mpa_include,
-    ${CACHE} as co,
-    ${MP} as mpo_include,
-    ${CACHE} as cs,
-    ${MP} as mps_include
-    WHERE (
-      ca."rule_id" != 0 AND
-      co."rule_id" != 0 AND
-      cs."rule_id" != 0 AND
-
-      ca."rule_id" = co."rule_id" AND
-      ca."rule_id" = cs."rule_id" AND
-
-      ca."rule_action_id" != 0 AND
-      ca."selector_include_id" != 0 AND
-      mpa_include."path_item_id" = ca."link_id" AND
-      mpa_include."group_id" = ca."tree_id" AND
-      ${/*NOT EXISTS (
-        SELECT mpa_exclude."id"
-        FROM
-        ${CACHE} as cache_exclude,
-        ${MP} as mpa_exclude
-        WHERE (
-          cache_exclude."selector_exclude_id" != 0 AND
-          cache_exclude."selector_id" = ca."selector_id" AND
-          mpa_exclude."path_item_id" = cache_exclude."link_id" AND
-          mpa_exclude."item_id" = mpa_include."item_id" AND
-          mpa_exclude."group_id" = cache_exclude."tree_id"
-        )
-      ) AND*/''}
-
-      co."rule_object_id" != 0 AND
-      co."selector_include_id" != 0 AND
-      mpo_include."path_item_id" = co."link_id" AND
-      mpo_include."group_id" = co."tree_id" AND
-      ${/*NOT EXISTS (
-        SELECT mpo_exclude."id"
-        FROM
-        ${CACHE} as cache_exclude,
-        ${MP} as mpo_exclude
-        WHERE (
-          cache_exclude."selector_exclude_id" != 0 AND
-          cache_exclude."selector_id" = co."selector_id" AND
-          mpo_exclude."path_item_id" = cache_exclude."link_id" AND
-          mpo_exclude."item_id" = mpo_include."item_id" AND
-          mpo_exclude."group_id" = cache_exclude."tree_id"
-        )
-      ) AND*/''}
-
-      cs."rule_subject_id" != 0 AND
-      cs."selector_include_id" != 0 AND
-      mps_include."path_item_id" = cs."link_id" AND
-      mps_include."group_id" = cs."tree_id"
-      ${/*NOT EXISTS (
-        SELECT mps_exclude."id"
-        FROM
-        ${CACHE} as cache_exclude,
-        ${MP} as mps_exclude
-        WHERE (
-          cache_exclude."selector_exclude_id" != 0 AND
-          cache_exclude."selector_id" = cs."selector_id" AND
-          mps_exclude."path_item_id" = cache_exclude."link_id" AND
-          mps_exclude."item_id" = mps_include."item_id" AND
-          mps_exclude."group_id" = cache_exclude."tree_id"
-        )
-      )*/''}
-    );
+      ${CACHE} co
+    JOIN 
+      ${MP} mpo_include ON mpo_include.path_item_id = co.link_id
+                    AND mpo_include.group_id = co.tree_id
+    JOIN
+      ${CACHE} ca ON ca.rule_id = co.rule_id
+    JOIN 
+      ${MP} mpa_include ON mpa_include.path_item_id = ca.link_id
+                    AND mpa_include.group_id = ca.tree_id
+    JOIN 
+      ${CACHE} cs ON ca.rule_id = cs.rule_id
+    JOIN 
+      ${MP} mps_include ON mps_include.path_item_id = cs.link_id
+                    AND mps_include.group_id = cs.tree_id
+    WHERE
+      ca."rule_id" != 0
   `);
   await api.query({
     type: 'track_table',
