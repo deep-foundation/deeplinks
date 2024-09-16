0cfd727659eec5e8b245063fdf580f6171409616
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-16 16:27:18 +0000

    0.0.520

--- imports/packages.ts
+++ imports/packages.ts
@@ -1,8 +1,9 @@
-import { DeepClient } from "./client";
-import { Id, Link } from "./minilinks";
+import { DeepClient } from "./client.js";
+import { Id, Link } from "./minilinks.js";
 import fs from 'fs';
 import path from 'path';
-import { Package } from "./packager";
+import { Package } from "./packager.js";
+import _ from 'lodash';;
 
 export class Packages {
   deep: DeepClient<Link<Id>>;
@@ -27,7 +28,7 @@ export class Packages {
       console.log('export package', `${p.id} ${p?.value?.value}`);
       const pckg = await packager.export({ packageLinkId: p.id });
       console.log('exported package', `${pckg.package.name} ${pckg.package.version}`);
-      results[`${pckg.package.name}@${pckg.package.version}`] = pckg;
+      results[_.camelCase(`${pckg.package.name}@${pckg.package.version}`)] = pckg;
     }
     return results;
   }
@@ -38,11 +39,11 @@ export class Packages {
     for (let i = 0; i < packages.length; i++) {
       const p = packages[i];
       fs.writeFileSync(
-        path.join(address, `${p?.package?.name}@${p.package.version}.json`.replace('/','@')),
+        path.join(address, _.camelCase(`${p?.package?.name}@${p.package.version}.json`)),
         JSON.stringify(p, null, 2),
         { encoding: 'utf-8' },
       );
-      console.log('writeed package', `${path.join(address, `${p?.package?.name}@${p.package.version}`)}`);
+      console.log('writeed package', `${path.join(address, _.camelCase(`${p?.package?.name}@${p.package.version}`))}`);
     }
   }
   async read(address: string): Promise<{ [name: string]: Package }> {
@@ -57,7 +58,7 @@ export class Packages {
         const json = fs.readFileSync(path.join(address, pckgs[i]), { encoding: 'utf-8' });
         try {
           const pckg = JSON.parse(json);
-          results[`${pckg.package.name}@${pckg.package.version}`] = pckg;
+          results[_.camelCase(`${pckg.package.name}@${pckg.package.version}`)] = pckg;
         } catch(e) {
           console.log('error read package', e);
         }
@@ -73,8 +74,8 @@ export class Packages {
     const results = {};
     for (let i = 0; i < packages.length; i++) {
       const p = packages[i];
-      console.log(`import package ${p.package.name}@${p.package.version}`);
-      results[`${p.package.name}@${p.package.version}`] = await packager.import(p);
+      console.log(`import package ${_.camelCase(`${p.package.name}@${p.package.version}`)}`);
+      results[_.camelCase(`${p.package.name}@${p.package.version}`)] = await packager.import(p);
     }
     return results;
   }
