608de4f000c5bf8b587d84164272111675cbfa77
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-24 12:12:58 +0300

    0.0.567

---
+++ migrations/1727169148530-payments.ts
@@ -0,0 +1,38 @@
+import { generateApolloClient } from '@deep-foundation/hasura/client.js';
+import Debug from 'debug';
+import { DeepClient } from '../imports/client.js';
+import { installPackage } from './1678940577209-deepcase.js';
+import { containWithin, packageExists, sharePermissions } from './1664940577200-tsx.js';
+
+const debug = Debug('deeplinks:migrations:payments');
+const log = debug.extend('log');
+const error = debug.extend('error');
+
+const rootClient = generateApolloClient({
+  path: `${process.env.MIGRATIONS_HASURA_PATH}/v1/graphql`,
+  ssl: !!+(process.env.MIGRATIONS_HASURA_SSL || 0),
+  secret: process.env.MIGRATIONS_HASURA_SECRET,
+});
+
+const root = new DeepClient({
+  apolloClient: rootClient,
+});
+
+export const up = async () => {
+  log('up');
+  const packageName = '@deep-foundation/payments';
+  if (!await packageExists(packageName)) {
+    const adminId = await root.id('deep', 'admin');
+    const admin = await root.login({ linkId: adminId });
+    const deep = new DeepClient({ deep: root, ...admin });
+
+    await installPackage(deep, '@deep-foundation/payments');
+    const packageId = await root.id('@deep-foundation/payments');
+    await sharePermissions(adminId, packageId);
+    await containWithin(adminId, packageId);
+  }
+};
+
+export const down = async () => {
+  log('down');
+};
